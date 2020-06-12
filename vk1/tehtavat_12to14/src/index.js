dcee36b13947e6c18b9a164fbbfac409747ed9de
Arttu Mykkänen <arttu.mykkanen@outlook.com>
2020-06-12 19:59:11 +0300

    Do 1.14

--- vk1/tehtavat_12to14/src/index.js
+++ vk1/tehtavat_12to14/src/index.js
@@ -6,14 +6,20 @@ const Button = ({ onClick, text }) =>
     {text}
   </button>
 
+const Header = ({ text }) =>
+  <h1>
+    {text}
+  </h1>
+
 const App = ( {anecdotes} ) => {
   const [selected, setSelected] = useState(0)
   const [votes, setVotes] = useState(Array.apply(null,
     new Array(anecdotes.length)).map(Number.prototype.valueOf, 0))
+  const [maxIndex, setMaxIndex] = useState(0)
 
   const getRandom = (a, b) => {
     const randomNum = Math.floor(Math.random() * (b - a) + a)
-    console.log(randomNum)
+    //console.log(randomNum)
     return randomNum
   }
 
@@ -22,17 +28,37 @@ const App = ( {anecdotes} ) => {
   }
 
   const handleButtonVote = () => {
-    const copy = { ...votes }
+    const copy = [ ...votes ]
     copy[selected]++
     setVotes(copy)
+    setMaxIndex(getMaxIndex(copy))
+  }
+
+  const getMaxIndex = (ccc) => {
+    if (ccc.length === 0)
+      return -1
+    let max = ccc[0]
+    let index = 0
+    //console.log("len: ", ccc.length)
+    for (let i = 1; i < ccc.length; i++) {
+      if (ccc[i] > max) {
+        index = i
+        max = ccc[i]
+      }
+    }
+    //console.log('max index: ', index)
+    return index
   }
 
   return (
     <div>
+      <Header text='Anecdote of the day' />
       {anecdotes[selected]} <br></br>
       has {votes[selected]} votes <br></br>
       <Button onClick={handleButtonVote} text='vote' />
       <Button onClick={handleButtonAnecdote} text='next anecdote' />
+      <Header text='Anecdote with most votes' />
+      {anecdotes[maxIndex]}
     </div>
   )
 }

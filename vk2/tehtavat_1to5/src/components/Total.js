3ce75165c27f1c046c0df566eb386b139d8abeea
Arttu Mykkänen <arttu.mykkanen@outlook.com>
2020-06-13 23:00:17 +0300

    Do 2.4

--- vk2/tehtavat_1to5/src/components/Total.js
+++ vk2/tehtavat_1to5/src/components/Total.js
@@ -3,7 +3,9 @@ import React from 'react'
 const Total = ({ parts }) =>
 (
   <p>
-    total of {parts.reduce((total, part) => total + part.exercises, 0)} exercises
+    <strong>
+      total of {parts.reduce((total, part) => total + part.exercises, 0)} exercises
+    </strong>
   </p>
 )
 export default Total

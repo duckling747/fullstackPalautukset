e248214b62df7ee7ac4e65be593d81ead0f4bdd1
Arttu Mykkänen <arttu.mykkanen@outlook.com>
2020-06-24 13:09:09 +0300

    Do 4.2

---
+++ vk4/app.js
@@ -0,0 +1,24 @@
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const blogsRouter = require("./controllers/blogs");
+const mongoose = require("mongoose");
+const logger = require("./utils/logger");
+
+
+mongoose
+    .connect(config.MONGODB_URI,
+        { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        logger.info("Connected to mongo...");
+    })
+    .catch(error => {
+        logger.error("Error connecting to mongo:", error.message);
+    });
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/blogs", blogsRouter);
+
+module.exports = app;

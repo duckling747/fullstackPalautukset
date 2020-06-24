9c4927ed809a44529dfa3b555cf447b51482bb3c
Arttu Mykkänen <arttu.mykkanen@outlook.com>
2020-06-24 14:39:59 +0300

    Do 4.5

--- vk4/utils/list_helper.js
+++ vk4/utils/list_helper.js
@@ -8,6 +8,12 @@ const totalLikes = blogs => {
     return blogs.reduce(sum, 0);
 };
 
+const favoriteBlog = blogs => {
+    return blogs.reduce(
+        (prev, current) => prev.likes > current.likes ? prev : current
+    );
+};
+
 module.exports = {
-    dummy, totalLikes
+    dummy, totalLikes, favoriteBlog
 };

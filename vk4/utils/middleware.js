bc9c6590f79de105dd22eebdd4754c25fba2a548
Arttu Mykkänen <arttu.mykkanen@outlook.com>
2020-06-28 13:23:15 +0300

    Do 4.20

--- vk4/utils/middleware.js
+++ vk4/utils/middleware.js
@@ -5,7 +5,6 @@ const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: "unknown endpoint" });
 };
   
-  
 const errorHandler = (error, request, response, next) => {
     if (error.name === "CastError") {
         return response.status(400).send( {error: "malformed id" });
@@ -16,12 +15,20 @@ const errorHandler = (error, request, response, next) => {
             error: "invalid token"
         });
     }
-    
+
     logger.error(error.message);
     
     next(error);
 };
 
+const tokenExtractor = (request, response, next) => {
+    const authorization = request.get("authorization");
+    if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+        request.token = authorization.substring(7);
+    }
+    next();
+};
+
 module.exports = {
-    unknownEndpoint, errorHandler
+    unknownEndpoint, errorHandler, tokenExtractor
 };

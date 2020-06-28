b78ff9d9c336eb27d2b1b7329acc346f00ee993b
Arttu Mykkänen <arttu.mykkanen@outlook.com>
2020-06-28 12:43:22 +0300

    Do 4.19

--- vk4/controllers/blogs.js
+++ vk4/controllers/blogs.js
@@ -1,7 +1,16 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
+
+const getTokenFrom = request => {
+    const authorization = request.get("authorization");
+    if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+        return authorization.substring(7);
+    }
+    return null;
+};
 
 blogsRouter.get("/", async (request, response) => {
     const blogs = await Blog.find({})
@@ -11,9 +20,14 @@ blogsRouter.get("/", async (request, response) => {
 
 blogsRouter.post("/", async (request, response) => {
     const body = request.body;
+    const token = getTokenFrom(request);
+    
+    const decodedToken = jwt.verify(token, process.env.SECRET);
+    if (!token || !decodedToken.id)
+        return response.status(401)
+            .json({ error: "token missing or invalid" });
 
-    //const user = await User.findById(body.userId);
-    const user = await User.findOne();
+    const user = await User.findById(decodedToken.id);
 
     const blog = new Blog({
         title: body.title,

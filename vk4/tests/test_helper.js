1e8c46fc2317dda13c54e4101dd7bc53db3eeacc
Arttu Mykkänen <arttu.mykkanen@outlook.com>
2020-06-28 18:44:41 +0300

    Do 4.22 +

--- vk4/tests/test_helper.js
+++ vk4/tests/test_helper.js
@@ -73,14 +73,6 @@ const newBlog = {
     __v: 0 
 };
 
-const newBlogNoLikes = { 
-    _id: "5a422ba71b54a676234d17f0", 
-    title: "Binary B-Trees for Virtual Memory", 
-    author: "Rudolf Bayer", 
-    url: "https://dl.acm.org/doi/10.1145/1734714.1734731",
-    __v: 0 
-};
-
 const blogsInDb = async () => {
     const blogs = await Blog.find({});
     return blogs.map(blog => blog.toJSON());

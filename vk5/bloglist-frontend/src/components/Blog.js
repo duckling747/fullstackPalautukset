cbd68f1471856e2033942e2fce266ad74c50c1bf
Arttu Mykkänen <arttu.mykkanen@outlook.com>
2020-07-03 19:24:34 +0300

    Make likes work even better and with sorting

--- vk5/bloglist-frontend/src/components/Blog.js
+++ vk5/bloglist-frontend/src/components/Blog.js
@@ -6,7 +6,9 @@ import blogs from '../services/blogs'
 const Blog = ({ blog }) => {
 
   const [detailed, setDetailed] = useState(false)
-  const [likes, setLikes] = useState(blog.likes)
+  const [s_blog, setBlog] = useState(
+    { ...blog }
+  )
 
   const blogStyle = {
     paddingTop: 10,
@@ -17,31 +19,36 @@ const Blog = ({ blog }) => {
     display: detailed ? '' : 'none'
   }
 
-  const sendLike = async (blog) => {
+  const sendLike = async () => {
     // const res = 
-    await blogs.update(blog.id, 
-      { ...blog, likes: blog.likes+1, user: blog.user.id })
-    // console.log(res)
-    setLikes(blog.likes+1)
+    const newBlog = { ...s_blog, likes: s_blog.likes+1, user: s_blog.user.id }
+    await blogs.update(newBlog.id, newBlog)
+    setBlog(
+    {
+      ...s_blog,
+      likes: s_blog.likes+1,
+      user: { ...s_blog.user }
+    }
+    )
   }
 
   return (
   <>
     <div style={blogStyle}>
-      {blog.title}; {blog.author} <br></br>
-      {blog.url} <br></br>
-      likes: {likes} {' '}
-      <button onClick={ () => sendLike(blog) }>like</button> <br></br>
+      {s_blog.title}; {s_blog.author} <br></br>
+      {s_blog.url} <br></br>
+      likes: {s_blog.likes} {' '}
+      <button onClick={ () => sendLike() }>like</button> <br></br>
       {
         blog.user
-        ? `${blog.user.username} (${blog.user.name})`
+        ? `${s_blog.user.username} (${s_blog.user.name})`
         : ''
       }
       {' '}
       <button onClick={ () => setDetailed(false) }>hide</button>
     </div>
     <div style={ {...blogStyle, display: detailed ? 'none' : ''} }>
-      {blog.title}; {blog.author} {' '}
+      {s_blog.title}; {s_blog.author} {' '}
       <button onClick={ () => setDetailed(true) }>view</button>
     </div>
   </>
--- vk5/bloglist-frontend/src/components/BlogList.js
+++ vk5/bloglist-frontend/src/components/BlogList.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Blog from './Blog'
 
+const comparator = (a, b) => {
+    return -(a.likes - b.likes)
+}
+
 const BlogList = ({ blogs }) => {
     return (
     <>
         <h2>Bloglist</h2>
-        {blogs.map(blog =>
+        {blogs
+        .sort(comparator)
+        .map(blog =>
             <Blog key={blog.id} blog={blog} />
         )}
     </>

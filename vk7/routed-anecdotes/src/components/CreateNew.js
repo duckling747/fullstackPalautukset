c98c2600898f4ba4ced98001c7edc81e4a65bea1
Arttu Mykkänen <arttu.mykkanen@outlook.com>
2020-07-14 20:06:19 +0300

    Do 7.4 & 7.5 & 7.6

--- vk7/routed-anecdotes/src/components/CreateNew.js
+++ vk7/routed-anecdotes/src/components/CreateNew.js
@@ -1,25 +1,33 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useHistory } from 'react-router-dom'
+import useField from '../hooks'
 
 let lastNotification = null
 
 const CreateNew = (props) => {
-    const [content, setContent] = useState('')
-    const [author, setAuthor] = useState('')
-    const [info, setInfo] = useState('')
+
+    const {reset: reset1, ...content} = useField('content')
+    const {reset: reset2, ...author} = useField('author')
+    const {reset: reset3, ...info} = useField('info')
+
+    const resetAll = () => {
+      reset1()
+      reset2()
+      reset3()
+    }
   
     const history = useHistory()
   
     const handleSubmit = (e) => {
       e.preventDefault()
       props.addNew({
-        content,
-        author,
-        info,
+        content: content.value,
+        author: author.value,
+        info: info.value,
         votes: 0
       })
       history.push('/')
-      props.setNotification(`a new anecdote ${content} created!`)
+      props.setNotification(`a new anecdote ${content.value} created!`)
       if (lastNotification) clearTimeout(lastNotification)
       lastNotification = setTimeout(() => {
         props.setNotification('')
@@ -33,17 +41,18 @@ const CreateNew = (props) => {
         <form onSubmit={handleSubmit}>
           <div>
             content
-            <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+            <input {...content} />
           </div>
           <div>
             author
-            <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+            <input {...author} />
           </div>
           <div>
             url for more info
-            <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+            <input {...info} />
           </div>
-          <button>create</button>
+          <button type='submit'>create</button>
+          <button type='button' onClick={resetAll}>reset</button>
         </form>
       </div>
     )

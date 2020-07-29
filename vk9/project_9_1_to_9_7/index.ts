9410b2c9fc6b10834cc6b26c9fedc06a0ae5dba1
Arttu Mykkänen <arttu.mykkanen@outlook.com>
2020-07-30 01:11:32 +0300

    Do 9.7 (WebExercises POST request)

--- vk9/project_9_1_to_9_7/index.ts
+++ vk9/project_9_1_to_9_7/index.ts
@@ -1,7 +1,12 @@
 import express from "express";
 import { calculateBmi } from './bmiCalculator';
+import { calculateExercises } from "./exerciseCalculator";
 
 const app = express();
+
+const erroneousParametersJSON = { error: "parameters missing" },
+  erroneousFormattingJSON = { error: "malformatted parameters" };
+
 
 app.get("/hello", (_req, res) => {
     res.send("Hello Full Stack");
@@ -10,14 +15,13 @@ app.get("/hello", (_req, res) => {
 app.get("/bmi", (req, res) => {
     const weight = req?.query?.weight;
     const height = req?.query?.height;
-    const errormessagejson = { error: "malformatted params" };
     if (!weight || !height) {
-        res.status(400).json(errormessagejson);
+        res.status(400).json(erroneousParametersJSON);
     } else {
         const parsew = +weight,
           parseh = +height;
         if (isNaN(parsew) || isNaN(parseh)) {
-            res.status(400).json(errormessagejson);
+            res.status(400).json(erroneousFormattingJSON);
         } else {
             res.json({
               weight,
@@ -26,6 +30,33 @@ app.get("/bmi", (req, res) => {
           });
         }
     }
+});
+
+app.use(express.json());
+
+interface Bodystuff {
+    target: number,
+    daily_exercises: Array<number>
+}
+
+
+app.post("/exercises", (request, response) => {
+    // Casting does not yet guarantee that the fields are numbers
+    const body: Bodystuff = request.body as Bodystuff;
+
+    // Do some typechecking etc. to make sure
+    if (!body.target || !body.daily_exercises)
+      response.json(erroneousParametersJSON);
+    const { target, daily_exercises } = body;
+    if (typeof target !== "number" || daily_exercises.constructor !== Array)
+      response.json(erroneousFormattingJSON);
+    for (const num of daily_exercises)
+      if (typeof num !== "number") response.json(erroneousFormattingJSON);
+
+    // exercise calculator already returns appropriate JSON
+    response.json(
+      calculateExercises(daily_exercises, target)
+    );
 });
 
 const PORT = 3002;

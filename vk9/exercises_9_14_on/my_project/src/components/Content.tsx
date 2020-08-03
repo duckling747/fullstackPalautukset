78534acf5d254e27a4c17414cc677783d35a387d
Arttu Mykkänen <arttu.mykkanen@outlook.com>
2020-08-03 16:24:45 +0300

    Do 9.14 (create-react-app w/ typescript)

---
+++ vk9/exercises_9_14_on/my_project/src/components/Content.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Course from '../types/Course'
+
+
+const Content: React.FC<{ courseParts: Array<Course> }> = ({ courseParts }) => {
+    return (
+    <>
+      <p>
+        {courseParts[0].name} {courseParts[0].exerciseCount}
+      </p>
+      <p>
+        {courseParts[1].name} {courseParts[1].exerciseCount}
+      </p>
+      <p>
+        {courseParts[2].name} {courseParts[2].exerciseCount}
+      </p>
+    </>
+    )
+}
+
+export default Content
---
+++ vk9/exercises_9_14_on/my_project/src/components/Total.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Course from '../types/Course'
+
+
+const Total: React.FC<{ courseParts: Array<Course> }> = ({ courseParts }) => {
+    return (
+    <p>
+        Number of exercises{" "}
+        {courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
+    </p>
+    )
+}
+
+export default Total
